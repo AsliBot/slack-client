0c182dfabdde50dde34351b991ba2fd120eb3ccc
Udit <codenirvana28@gmail.com>
2017-03-22 14:40:38 +0530

    updated params name

--- index.js
+++ index.js
@@ -3,22 +3,17 @@ const axios = require('axios');
 const env = process.env.NODE_ENV || "DEV";
 const config = require('./config')[env];
 
-const controller = Botkit.slackbot({
-    debug: false
-});
+const controller = Botkit.slackbot({ debug: false });
 
 controller.spawn({
-    token: config.token
+  token: config.token
 }).startRTM((err, bot, payload) => {
-    if (err) {
-        throw new Error('Error connecting to slack: ', err);
-    }
-    console.log('Connected to Slack');
+  if (err) throw new Error('Error connecting to slack: ', err);
+  console.log('Connected to Slack');
 });
 
 controller.hears('', ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-
-  axios.get( `${config.asliBot}?s=${message.user}&q=${message.text}` )
+  axios.get( `${config.asliBot}?q=${message.text}&platform=slack&user=${message.user}` )
     .then( res => {
       const response = res.data;
       if(response.error) {
